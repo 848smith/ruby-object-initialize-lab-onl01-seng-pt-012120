fa9dca6732a0249694ad5b12cf52c4ef5165a50a
Andrew Smith <848smith@gmail.com>
2020-02-11 07:44:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,10 @@
 class Dog
   def initialize(name, breed)
     @name = name
-    @breed = breed
+    if breed = ""
+      @breed = "Mutt"
+    else
+      @breed = breed
   end
   
   def name=(new_name)
